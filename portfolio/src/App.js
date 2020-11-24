ef1dcdac064f428233a486ca769c92bdafb5398f
AK <icecicle04@gmail.com>
2020-11-24 11:52:59 -0500

    refactored a little bit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,44 +1,45 @@
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div style={{ height: "300px", position: "relative" }}>
-        <Layout fixedHeader>
-          <Header
-            title={
-              <span>
-                <span style={{ color: "#ddd" }}>Ali Khan's</span>
-                <strong> Portfolio</strong>
-              </span>
-            }
-          >
-            <Navigation>
-              <a href="/">Home</a>
-              <a href="/about">About Me</a>
-              <a href="/contact">Contact</a>
-              <a href="/projects">Projects</a>
-              <a href="/resume">Resume</a>
-            </Navigation>
-          </Header>
-          <Drawer title="Title">
-            <Navigation>
-              <a href="/">Home</a>
-              <a href="/about">About Me</a>
-              <a href="/contact">Contact</a>
-              <a href="/projects">Projects</a>
-              <a href="/resume">Resume</a>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
+      {/* <div style={{ height: "300px", position: "relative" }}> */}
+      <Layout fixedHeader>
+        <Header
+          title={
+            <span>
+              <span style={{ color: "#ddd" }}>Ali Khan's</span>
+              <strong> Portfolio</strong>
+            </span>
+          }
+        >
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Me</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/resume">Resume</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="">
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Me</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/resume">Resume</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+      {/* </div> */}
     </div>
   );
 }
